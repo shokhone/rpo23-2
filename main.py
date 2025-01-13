c7a074d521239328a2f57f7903c5bd51939307ce
sobirjonov_sh <sobirjonovshoh@gmail.com>
2025-01-13 11:41:36 +0200

    0.1

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
-class Student:
-    def __init__(self, name, age):
+# class Student:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def say_hello(self):
+#         print(f"Hello! My Name is {self.name}")
+#
+#     def get_info(self):
+#         return f"{self.name}; {self.age}"
+#
+# vasya = Student("Vasya",15)
+# petya = Student("Petya",17)
+# print(vasya)
+# print(vasya.name,vasya.age)
+# print(petya.name,petya.age)
+# vasya.say_hello()
+# print(vasya.get_info())
+# del vasya
+# print(vasya.name)
+
+class Animal:
+    animal_counter = 0
+
+    def __init__(self,animal_type, name):
+        self.animal_type = animal_type
         self.name = name
-        self.age = age
+        Animal.animal_counter += 1
 
-    def say_hello(self):
-        print(f"Hello! My Name is {self.name}")
+    def show_info(self):
+        print(f"{self.animal_type};{self.name}")
 
-    def get_info(self):
-        return f"{self.name}; {self.age}"
-
-vasya = Student("Vasya",15)
-petya = Student("Petya",17)
-print(vasya)
-print(vasya.name,vasya.age)
-print(petya.name,petya.age)
-vasya.say_hello()
-print(vasya.get_info())
-del vasya
-print(vasya.name)
-
+print(Animal.animal_counter)
+barsik = Animal("Cat","Barsik")
+sharik = Animal("Dog","Sharik")
+barsik.show_info()
+print(barsik.animal_counter)
