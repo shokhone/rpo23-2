39071177aa153a99f1f6ebf1abea1ad01e3b8d03
sobirjonov_sh <sobirjonovshoh@gmail.com>
2025-01-20 11:39:43 +0200

    0.1

--- main.py
+++ main.py
@@ -19,6 +19,9 @@
 # del vasya
 # print(vasya.name)
 
+
+
+
 # class Animal:
 #     animal_counter = 0
 #
@@ -147,37 +150,137 @@
 #         super().__init__(name, year)
 #         self.group = group
 
+#
+# class Weather:
+#
+#     @staticmethod
+#     def celsius_to_fahrenheit(t):
+#         return t * 9/5 + 32
+#
+#     @staticmethod
+#     def celsius_to_kelvins(t):
+#         return t + 235.15
+#
+#     @staticmethod
+#     def fahrenheit_to_celsius(t):
+#         return (t - 32) * 5/9
+#
+#     @staticmethod
+#     def fahrenheit_to_kelvins(t):
+#         return (t - 32) * 5/9 + 273.15
+#
+#     @staticmethod
+#     def kelvins_to_celsius(t):
+#         return t - 273.15
+#
+#     @staticmethod
+#     def kelvins_to_fahrenheit(t):
+#         return (t - 273.15) * 9/5 + 32
+#
+#
+# print(Weather.celsius_to_fahrenheit(6))
+# print(Weather.celsius_to_kelvins(9))
+# print(Weather.fahrenheit_to_celsius(34))
+# print(Weather.fahrenheit_to_kelvins(10))
+# print(Weather.kelvins_to_celsius(9))
+# print(Weather.kelvins_to_celsius(12))
 
-class Weather:
+# class Fraction:
+#     def __init__(self,numerator, denominator):
+#         self.a = numerator
+#         self.b = denominator
+#
+#     def __str__(self):
+#         return f"{self.a}/{self.b}"
+#
+#     def summ(self,other):
+#         return Fraction(self.a * other.b + self.b * other.a, self.b * other.b)
+#
+#     def subraction(self,other):
+#         return Fraction(self.a * other.b - self.b * other.a, self.b * other.b)
+#
+#     def multiplicaton(self,other):
+#         return Fraction(self.a * other.a , self.b * other.b)
+#
+#     def division(self,other):
+#         return Fraction(self.a * other.b , self.b * other.b)
+#
+#     def __add__(self, other):
+#         return Fraction(self.a * other.b + self.b * other.a, self.b * other.b)
+#
+#     def __sub__(self, other):
+#         return Fraction(self.a * other.b - self.b * other.a, self.b * other.b)
+#
+#     def __mul__(self, other):
+#         return Fraction(self.a * other.a , self.b * other.b)
+#
+#
+# first = Fraction(2, 5)
+# print(first)
+# second = Fraction(2,3)
+# print(first.summ(second))
+# print(first.subraction(second))
+# print(first.multiplicaton(second))
+# print(first.division(second))
+# print(first + second)
+# print(first - second)
 
-    @staticmethod
-    def celsius_to_fahrenheit(t):
-        return t * 9/5 + 32
+# from math import factorial
+#
+# class MyMath:
+#
+#     @staticmethod
+#     def math_max(a,b,d,c):
+#         return max(a,b,d,c)
+#
+#     @staticmethod
+#     def math_nin(a,b,d,c):
+#         return min(a,b,d,c)
+#
+#     @staticmethod
+#     def sum_math(a,b,d,c):
+#         return (a+b+d+c)/4
+#
+#     @staticmethod
+#     def Fakt_math(t):
+#         return factorial(t)
+#
+# print(MyMath.sum_math(5,6,7,8))
 
-    @staticmethod
-    def celsius_to_kelvins(t):
-        return t + 235.15
+from abc import ABC, abstractmethod
 
-    @staticmethod
-    def fahrenheit_to_celsius(t):
-        return (t - 32) * 5/9
+class Figure2D(ABC):
 
-    @staticmethod
-    def fahrenheit_to_kelvins(t):
-        return (t - 32) * 5/9 + 273.15
+    @abstractmethod
+    def area(self):
+        pass
 
-    @staticmethod
-    def kelvins_to_celsius(t):
-        return t - 273.15
+    @abstractmethod
+    def perimetr(self):
+        pass
 
-    @staticmethod
-    def kelvins_to_fahrenheit(t):
-        return (t - 273.15) * 9/5 + 32
+class Square(Figure2D):
+    def __init__(self, side):
+        self.side = side
+
+     def area(self):
+         return self.side ** 2
+
+    def perimetr(self):
+        return self.side * 4
 
 
-print(Weather.celsius_to_fahrenheit(6))
-print(Weather.celsius_to_kelvins(9))
-print(Weather.fahrenheit_to_celsius(34))
-print(Weather.fahrenheit_to_kelvins(10))
-print(Weather.kelvins_to_celsius(9))
-print(Weather.kelvins_to_celsius(12))
+class Reactangle(Figure2D):
+    def __init__(self, width,heght):
+        self.width = width
+
+    def area(self):
+        return self.side ** 2
+
+    def perimetr(self):
+        return self.side * 4
+
+
+
+sponge_bob = Square(5)
+sponge_bob.area()
